f8a0cbe1a53e3b43a88add25b4ca6f0865bddcae
T93076\user <93076@mail.wzu.edu.tw>
2024-08-06 15:40:35 +0800

    week1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@ import VueWeek1 from './components/VueWeek1.vue'
 </script>
 
 <template>
-  <h1>App.Vue</h1>
   <VueWeek1></VueWeek1>
 </template>
 
--- src/components/VueWeek1.vue
+++ src/components/VueWeek1.vue
@@ -1,4 +1,67 @@
+<script setup>
+import { ref } from 'vue'
+
+const drinks = ref([
+  {
+    id: 1,
+    item: '珍珠奶茶',
+    desc: '香濃奶茶搭配QQ珍珠',
+    price: 50,
+    qty: 20
+  },
+  {
+    id: 2,
+    item: '冬瓜檸檬',
+    desc: '清新冬瓜配上新鮮檸檬',
+    price: 45,
+    qty: 18
+  },
+  {
+    id: 3,
+    item: '翡翠檸檬',
+    desc: '綠茶與檸檬的完美結合',
+    price: 55,
+    qty: 34
+  },
+  {
+    id: 4,
+    item: '四季春茶',
+    desc: '香醇四季春茶，回甘無比',
+    price: 45,
+    qty: 10
+  },
+  {
+    id: 5,
+    item: '阿薩姆奶茶',
+    desc: '阿薩姆紅茶搭配香醇鮮奶',
+    price: 50,
+    qty: 25
+  },
+  {
+    id: 6,
+    item: '檸檬冰茶',
+    desc: '檸檬與冰茶的清新組合',
+    price: 45,
+    qty: 20
+  },
+  {
+    id: 7,
+    item: '芒果綠茶',
+    desc: '芒果與綠茶的獨特風味',
+    price: 55,
+    qty: 18
+  },
+  {
+    id: 8,
+    item: '抹茶拿鐵',
+    desc: '抹茶與鮮奶的絕配',
+    price: 60,
+    qty: 20
+  }
+])
+</script>
 <template>
+  <h1>Week1</h1>
   <div>
     <table>
       <thead>
@@ -10,53 +73,16 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>珍珠奶茶</td>
-          <td><small>香濃奶茶搭配QQ珍珠</small></td>
-          <td>50</td>
-          <td><button>-</button>20<button>+</button></td>
-        </tr>
-        <tr>
-          <td>冬瓜檸檬</td>
-          <td><small>清新冬瓜配上新鮮檸檬</small></td>
-          <td>45</td>
-          <td><button>-</button>18<button>+</button></td>
-        </tr>
-        <tr>
-          <td>翡翠檸檬</td>
-          <td><small>綠茶與檸檬的完美結合</small></td>
-          <td>55</td>
-          <td><button>-</button>34<button>+</button></td>
-        </tr>
-        <tr>
-          <td>四季春茶</td>
-          <td><small>香醇四季春茶，回甘無比</small></td>
-          <td>45</td>
-          <td><button>-</button>10<button>+</button></td>
-        </tr>
-        <tr>
-          <td>阿薩姆奶茶</td>
-          <td><small>阿薩姆紅茶搭配香醇鮮奶</small></td>
-          <td>50</td>
-          <td><button>-</button>25<button>+</button></td>
-        </tr>
-        <tr>
-          <td>檸檬冰茶</td>
-          <td><small>檸檬與冰茶的清新組合</small></td>
-          <td>45</td>
-          <td><button>-</button>20<button>+</button></td>
-        </tr>
-        <tr>
-          <td>芒果綠茶</td>
-          <td><small>芒果與綠茶的獨特風味</small></td>
-          <td>55</td>
-          <td><button>-</button>18<button>+</button></td>
-        </tr>
-        <tr>
-          <td>抹茶拿鐵</td>
-          <td><small>抹茶與鮮奶的絕配</small></td>
-          <td>60</td>
-          <td><button>-</button>20<button>+</button></td>
+        <tr v-for="(item, key) in drinks" :key="item.id">
+          <td>{{ item.item }}</td>
+          <td>
+            <small>{{ item.desc }}</small>
+          </td>
+          <td>{{ item.price }}</td>
+          <td>
+            <button type="button" @click="item.qty > 0 ? item.qty-- : 0">-</button>{{ item.qty
+            }}<button type="button" @click="item.qty++">+</button>
+          </td>
         </tr>
       </tbody>
     </table>
